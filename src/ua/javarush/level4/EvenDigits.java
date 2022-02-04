52be93947aec12d1f909234f7554e6de257a2ebc
Babiichuk <heorhii.babiichuk@gmail.com>
2022-02-04 11:50:56 +0200

    3 Tasks for Module 4 Javarush

---
+++ src/ua/javarush/level4/EvenDigits.java
@@ -0,0 +1,13 @@
+package ua.javarush.level4;
+
+public class EvenDigits {
+
+    public static void main(String[] args) {
+        for (int i =1; i<=15; i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+
+}
